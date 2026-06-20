7cae6f1ce5dd67dbf5100bd2f0057ec145c7001f
mykhalenych <mykhalenych@gmail.com>
2026-06-20 09:51:40 +0300

    added solution

--- src/main/java/mate/academy/lib/Injector.java
+++ src/main/java/mate/academy/lib/Injector.java
@@ -1,1 +1,111 @@
+package mate.academy.lib;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Injector {
+    private static final Injector injector = new Injector();
+    private final Map<Class<?>, Object> instances = new HashMap<>();
+    private final Map<Class<?>, Class<?>> interfaceImplementations = new HashMap<>();
+
+    private Injector() {
+        try {
+            List<Class<?>> classes = getClasses("mate.academy");
+            for (Class<?> clazz : classes) {
+                if (clazz.isAnnotationPresent(Component.class)) {
+                    Class<?>[] interfaces = clazz.getInterfaces();
+                    if (interfaces.length > 0) {
+                        interfaceImplementations.put(interfaces[0], clazz);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new InjectionException("Can't initialize injector", e);
+        }
+    }
+
+    public static Injector getInjector() {
+        return injector;
+    }
+
+    public Object getInstance(Class<?> interfaceClazz) {
+        if (instances.containsKey(interfaceClazz)) {
+            return instances.get(interfaceClazz);
+        }
+        Class<?> clazz = findImplementation(interfaceClazz);
+        if (clazz == null) {
+            throw new InjectionException("No implementation found for " + interfaceClazz.getName());
+        }
+        if (!clazz.isAnnotationPresent(Component.class)) {
+            throw new InjectionException("Missing @Component annotation on class " 
+                    + clazz.getName());
+        }
+        try {
+            Constructor<?> constructor = clazz.getConstructor();
+            Object instance = constructor.newInstance();
+            Field[] fields = clazz.getDeclaredFields();
+            for (Field field : fields) {
+                if (field.isAnnotationPresent(Inject.class)) {
+                    Object fieldInstance = getInstance(field.getType());
+                    field.setAccessible(true);
+                    field.set(instance, fieldInstance);
+                }
+            }
+            instances.put(interfaceClazz, instance);
+            return instance;
+        } catch (ReflectiveOperationException e) {
+            throw new InjectionException("Can't create instance of " + clazz.getName(), e);
+        }
+    }
+
+    private Class<?> findImplementation(Class<?> interfaceClazz) {
+        if (interfaceClazz.isInterface()) {
+            return interfaceImplementations.get(interfaceClazz);
+        }
+        return interfaceClazz;
+    }
+
+    private static List<Class<?>> getClasses(String packageName)
+            throws ClassNotFoundException, IOException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        String path = packageName.replace('.', '/');
+        Enumeration<URL> resources = classLoader.getResources(path);
+        List<File> dirs = new ArrayList<>();
+        while (resources.hasMoreElements()) {
+            URL resource = resources.nextElement();
+            dirs.add(new File(resource.getFile()));
+        }
+        ArrayList<Class<?>> classes = new ArrayList<>();
+        for (File directory : dirs) {
+            classes.addAll(findClasses(directory, packageName));
+        }
+        return classes;
+    }
+
+    private static List<Class<?>> findClasses(File directory, String packageName)
+            throws ClassNotFoundException {
+        List<Class<?>> classes = new ArrayList<>();
+        if (!directory.exists()) {
+            return classes;
+        }
+        File[] files = directory.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                classes.addAll(findClasses(file, packageName + "." + file.getName()));
+            } else if (file.getName().endsWith(".class")) {
+                String className = packageName + '.'
+                        + file.getName().substring(0, file.getName().length() - 6);
+                classes.add(Class.forName(className));
+            }
+        }
+        return classes;
+    }
+}
